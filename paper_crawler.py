d6d466ce109797a64a67317d9a834849f0225397
mrowl14 <baothaitran13579@gmail.com>
2022-05-04 15:01:28 +0700

    new task 1

--- paper_crawler.py
+++ paper_crawler.py
@@ -1,48 +1,71 @@
+from urllib.error import HTTPError
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import urllib.request
 
-def crawl(save_dir, arthur_name):
-    # Tùy chỉnh browser để có thể tải files
-    options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory' : save_dir}
-    options.add_experimental_option("prefs", prefs)
+class PaperCrawler():
+    def __init__(self, storage):
+        self.storage = storage
+        self.browser = self.createBrowser()
+    
+    def createBrowser(self):
+        """ 
+            Khỏi tạo browser
+        """
+        browser = webdriver.Chrome('chromedriver.exe')
+        browser.get('https://scholar.google.com/')
+        sleep(3)
 
-    # Truy cập trang web
-    browser = webdriver.Chrome('chromedriver.exe', chrome_options=options)
-    browser.get('https://scholar.google.com/')
-    sleep(3)
+        return browser
 
-    # Tìm kiếm bài báo của tác giả
-    txtFind = browser.find_element_by_xpath('/html/body/div/div[7]/div[1]/div[2]/form/div/input')
-    txtFind.send_keys(arthur_name + ' paper')
-    txtFind.send_keys(Keys.ENTER)
-    sleep(3)
+    def searchPaper(self, author):
+        """
+            Tìm thanh tìm kiếm và nhập từ khóa cần tìm
+        """
+        txtFind = self.browser.find_element_by_xpath('/html/body/div/div[7]/div[1]/div[2]/form/div/input')
+        txtFind.send_keys(author + ' paper')
+        txtFind.send_keys(Keys.ENTER)
+        sleep(3)
+    
+    def crawlPaper(self):
+        """
+            1. Tìm những khối <div>..</div> có class là 'gs_r gs_or gs_scl' vì chúng sẽ chưa link truy cập đến bài báo
+            2. Lọc ra những link đến từ trang arxiv.org để tải không cần đăng nhập
+            3. Thực hiện lấy tên và đường dẫn.
+            4. Lưu bài báo tên tìm được.
+        """
+        paperList = self.browser.find_elements_by_xpath("//*[@class='gs_r gs_or gs_scl']") 
+        for paper in paperList:
+            # Tải pdf ở trang arxiv.org
+            paperLink = paper.find_element_by_class_name('gs_or_ggsm')
+            if paperLink.text.find('arxiv.org') == -1:
+                continue
 
-    # Tải các bài báo
-    paperList = browser.find_elements_by_xpath("//*[@class='gs_r gs_or gs_scl']")
-    for paper in paperList:
-        # Chỉ tải file pdf được ở trang arxiv.org
-        paperLink = paper.find_element_by_class_name('gs_or_ggsm')
-        if paperLink.text.find('arxiv.org') == -1: 
-            continue
-        # Lấy đường dẫn bài báo
-        link = paper.find_element_by_tag_name('a').get_attribute('href')
-        # Lấy tên bài báo
-        paperName = paper.find_element_by_class_name('gs_rt')
-        name = paperName.text
+            # Lấy đường dẫn bài báo
+            link = paper.find_element_by_tag_name('a').get_attribute('href')
 
-        # Lưu bài báo theo tên
-        response = urllib.request.urlopen(link)
-        file_name = "./files/" + "".join(x for x in name if x.isalnum() or x in [' ', '-']) + ".pdf"
-        file = open(file_name, 'wb')
-        file.write(response.read())
-        file.close()
+            # Lấy tên bài báo
+            paperName = paper.find_element_by_class_name('gs_rt')
+            name = paperName.text
 
-    sleep(3)
-    browser.close()
+            # Lưu bài báo theo tên
+            try:
+                response = urllib.request.urlopen(link)
+                file_name = self.storage + "".join(x for x in name if x.isalnum() or x in [' ', '-']) + ".pdf"
+                with open(file_name, 'wb') as f:
+                    f.write(response.read())
+            except HTTPError:
+                continue
+
+            sleep(3)
+
+    def crawl(self, author):
+        self.searchPaper(author)
+        self.crawlPaper()
+        self.browser.close()
+
 
 if __name__ == '__main__':
-    crawl('D:\Workspace\crawler\files',
-            'ian goodfellow')
+    crawler = PaperCrawler('./files/')
+    crawler.crawl('ian goodfellow')
